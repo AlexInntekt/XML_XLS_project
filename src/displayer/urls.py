ad3588859b858bddc789627fa4f86220a06784f3
alex <alex.inntekt@gmail.com>
2020-12-05 01:25:57 +0200

    save progress

--- src/displayer/urls.py
+++ src/displayer/urls.py
@@ -5,12 +5,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import Tools, Refresher, DetailView
+from .views import Tools, AddTool, Refresher, DetailView
 
 urlpatterns = [
     path('tools', Tools.as_view()),
     path('tools/category=<str:category>', Tools.as_view(), name='category'),
     path('tools/last_days=<str:last_days>', Tools.as_view()),
     path('tools/id=<int:id>', DetailView.as_view()),
+    path('tools/add_tool', AddTool.as_view()),
     path('refresh', Refresher.as_view()),
 ] 
